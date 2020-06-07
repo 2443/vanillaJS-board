bbe847c2e6c9fc1292c324b7f1bb927796c1b20b
2443 <condole1209@gmail.com>
2020-06-07 20:10:36 +0900

    apply prettier

--- script/module.js
+++ script/module.js
@@ -1,5 +1,5 @@
 const module = {
-    xhr: function({
+    xhr: function ({
         path,
         method,
         data,
@@ -8,16 +8,16 @@ const module = {
             const defaultUrl = 'http://localhost:3000/';;
             const xhr = new XMLHttpRequest();
             let url = defaultUrl + path;
-            if(!this.isEmpty(data) && method === 'get') {
+            if (!this.isEmpty(data) && method === 'get') {
                 url += '?' + this.jsonToQueryString(data);
             }
             xhr.open(method, url);
             xhr.onreadystatechange = () => {
-                if(xhr.readyState === 4 && xhr.status === 200){
+                if (xhr.readyState === 4 && xhr.status === 200) {
                     resolve(JSON.parse(xhr.responseText));
                 }
             }
-            
+
             if (!this.isEmpty(data) && ['post', 'delete', 'PATCH'].indexOf(method)) {
                 xhr.setRequestHeader('Content-Type', 'application/json'); // 컨텐츠타입을 json으로
                 xhr.send(JSON.stringify(data)); // 데이터를 stringify해서 보냄
@@ -26,7 +26,7 @@ const module = {
             }
         })
     },
-    queryStringToJson: function(queryString) {
+    queryStringToJson: function (queryString) {
         const json = {}
         queryString.split('&').forEach(element => {
             const keyValue = element.split('=');
@@ -36,15 +36,14 @@ const module = {
     },
     jsonToQueryString: json => {
         const querys = [];
-            for (let [key, value] of Object.entries(json)) {
-                querys.push(`${key}=${value}`);
-            }
-        console.log(querys)
+        for (let [key, value] of Object.entries(json)) {
+            querys.push(`${key}=${value}`);
+        }
         return querys.join('&');
     },
     isEmpty: data => {
         for (let key in data) {
-            if(key) {
+            if (key) {
                 return false;
             }
         }
